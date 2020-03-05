0640663032ea0cb89b30e4537bba76e1f791285d
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-05 12:21:23 +0100

    A script for full env provisioning

---
+++ scripts/build-full-env.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+CHAOS_ENGINE_INSTANCE_NANE="chaos-engine"
+CHAOS_ENGINE_VICTIM_NAME="$CHAOS_ENGINE_INSTANCE_NANE-victim"
+CHAOS_ENGINE_NETWORK_TAGS=$CHAOS_ENGINE_INSTANCE_NANE
+CHAOS_ENGINE_FW_NAME="$CHAOS_ENGINE_INSTANCE_NANE-inbound"
+CHAOS_ENGINE_ZONE="europe-west2-c"
+OPENED_PORTS="tcp:8080,tcp:8200,tcp:9000,tcp:5222,tcp:5280,tcp:5269,tcp:8089"
+
+
+SCRIPT_LOCATION=$(readlink -f $(dirname $0))
+
+function provision_chaos_machine() {
+  gcloud compute \
+    instances create $CHAOS_ENGINE_INSTANCE_NANE \
+    --zone=$CHAOS_ENGINE_ZONE \
+    --machine-type=n1-standard-1 \
+    --no-service-account \
+    --no-scopes \
+    --tags=$CHAOS_ENGINE_NETWORK_TAGS \
+    --image=ubuntu-1604-xenial-v20200129 \
+    --image-project=ubuntu-os-cloud \
+    --boot-disk-size=20GB \
+    --boot-disk-type=pd-standard \
+    --boot-disk-device-name=$CHAOS_ENGINE_INSTANCE_NANE \
+    --metadata-from-file startup-script=$SCRIPT_LOCATION/provision-vm.sh
+
+    gcloud compute ssh --zone $CHAOS_ENGINE_ZONE $CHAOS_ENGINE_INSTANCE_NANE --command "
+      sudo usermod -a -G docker $USER
+      cd
+      git clone https://github.com/luborpetr/chaos-engine-workshop.git
+      "
+}
+
+function setup_firewall() {
+  gcloud compute firewall-rules create $CHAOS_ENGINE_FW_NAME \
+    --direction=INGRESS \
+    --priority=1000 \
+    --network=default \
+    --action=ALLOW \
+    --rules=$OPENED_PORTS \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags=$CHAOS_ENGINE_NETWORK_TAGS
+
+}
+
+function deploy_chaos_engine() {
+    gcloud compute ssh --zone $CHAOS_ENGINE_ZONE $CHAOS_ENGINE_INSTANCE_NANE --command "
+      cd
+      git clone https://github.com/gemalto/chaos-engine.git
+      cd chaos-engine
+      wget -O docker-compose.yml https://raw.githubusercontent.com/luborpetr/chaos-engine-workshop/master/docker/docker-compose.yml
+      docker-compose pull
+    "
+}
+
+function deploy_k8s_cluster() {
+    gcloud container clusters create $CHAOS_ENGINE_VICTIM_NAME \
+    --zone $CHAOS_ENGINE_ZONE \
+    --no-enable-basic-auth \
+    --cluster-version "1.14.10-gke.17" \
+    --machine-type "g1-small" \
+    --image-type "COS" \
+    --disk-type "pd-standard" \
+    --disk-size "20" \
+    --num-nodes "3" \
+    --enable-stackdriver-kubernetes \
+    --enable-ip-alias \
+    --enable-autoupgrade \
+    --enable-autorepair
+
+    kubectl apply -f $SCRIPT_LOCATION/../kubernetes/rbac.yml
+
+}
+
+function deploy_workload() {
+    gcloud container clusters get-credentials $CHAOS_ENGINE_VICTIM_NAME --zone $CHAOS_ENGINE_ZONE
+
+    kubectl apply -f $SCRIPT_LOCATION/../kubernetes/applications.yml
+
+}
+
+function full_install() {
+    provision_chaos_machine
+    deploy_chaos_engine
+    setup_firewall
+    deploy_k8s_cluster
+    deploy_workload
+
+}
+
+$1
+
