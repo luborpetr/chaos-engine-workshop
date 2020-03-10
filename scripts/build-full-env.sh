d99847ecfe260516175432151e371a65e844de08
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-10 16:45:26 +0100

    Replace workloads

--- scripts/build-full-env.sh
+++ scripts/build-full-env.sh
@@ -84,7 +84,7 @@ function deploy_k8s_cluster() {
 function deploy_workload() {
     gcloud container clusters get-credentials $CHAOS_ENGINE_VICTIM_NAME --zone $CHAOS_ENGINE_ZONE
 
-    kubectl apply -f $SCRIPT_LOCATION/../kubernetes/applications.yml
+    kubectl apply -f $SCRIPT_LOCATION/../kubernetes/hipster_shop.yml
 
 }
 
