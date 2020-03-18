9ae5aebb7dddc0d740d50d1fc249ea9453169fea
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-18 10:12:00 +0100

    Added jabber server config script

---
+++ scripts/configuration/configure-jabber.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -it --rm -d --name ejabberd  \
+    -p 5222:5222 \
+    -p 5269:5269 \
+    -p 5443:5443 \
+    -p 1883:1883 \
+    -p 5280:5280 ejabberd/ecs
+
+
+docker exec -it ejabberd bin/ejabberdctl register test localhost test
+docker exec -it ejabberd bin/ejabberdctl register chaos localhost test
