b920715c3159cabd1cf25e60abdd4ae9d851140a
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-18 13:13:06 +0100

    Automate jabber provisioning

--- scripts/configuration/configure-jabber.sh
+++ scripts/configuration/configure-jabber.sh
@@ -2,6 +2,8 @@
 
 #Start and provision XMPP test server
 
+SCRIPT_LOCATION=$(readlink -f $(dirname $0))
+
 docker run -it --rm -d --name ejabberd  \
     -p 5222:5222 \
     -p 5269:5269 \
@@ -9,6 +11,14 @@ docker run -it --rm -d --name ejabberd  \
     -p 1883:1883 \
     -p 5280:5280 ejabberd/ecs
 
+sleep 5
+
+MACHINE_IP=$(SCRIPT_LOCATION/../get-ip.sh)
+
+docker exec -it ejabberd sed -i "s/-\ localhost/-\ localhost\n\  - $MACHINE_IP/g" /home/ejabberd/conf/ejabberd.yml
+docker exec -it ejabberd bin/ejabberdctl reload_config
+
+sleep 5
 
 docker exec  ejabberd bin/ejabberdctl register test localhost test
 docker exec  ejabberd bin/ejabberdctl register chaos localhost test
