ade82d9cd4e702b7557eb55fdcb92ccf0074b19f
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-18 09:23:53 +0100

    VNC config script

---
+++ vnc/configure-vnc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PASSWD="firefox"
+
+mkdir /home/$USER/.vnc
+echo $PASSWD | vncpasswd -f > /home/$USER/.vnc/passwd
+chown -R $USER:$USER /home/$USER/.vnc
+chmod 0600 /home/$USER/.vnc/passwd
+
+vncserver -desktop xfce -geometry 1280x720
+
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8085 localhost:5901
